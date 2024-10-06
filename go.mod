995de6d9a7479165d664a3af26acd9647476bd27
kamalsidhu001 <sidhukamla62398@gmail.com>
2024-10-06 09:30:52 -0400

    add go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kamalsidhu001/w6_go_2
+
+go 1.23.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Task struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"` // "pending" or "completed"
+}
+
+var tasks []Task
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range tasks {
+
+		if item.ID == params["id"] {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range tasks {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var task Task
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	task.ID = strconv.Itoa(rand.Intn(1000000))
+	tasks = append(tasks, task)
+	json.NewEncoder(w).Encode(task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Contant-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range tasks {
+		if item.ID == params["id"] {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			var task Task
+			_ = json.NewDecoder(r.Body).Decode(&task)
+			task.ID = params["id"]
+			tasks = append(tasks, task)
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	tasks = append(tasks, Task{ID: "1", Title: "Assignment 1", Description: "Cloud Security", Status: "Pending"})
+	tasks = append(tasks, Task{ID: "2", Title: "assignment 2", Description: "Network Security", Status: "Completed"})
+	r.HandleFunc("/tasks", getTasks).Methods("GET")
+	r.HandleFunc("/task/{id}", getTask).Methods("GET")
+	r.HandleFunc("/createtask", createTask).Methods("POST")
+	r.HandleFunc("/updatetask/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/deletetask/{id}", deleteTask).Methods("DELETE")
+	fmt.Printf("Starting server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
